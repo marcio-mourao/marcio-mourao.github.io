a60aeaf7c0fbaecec8f253861886ddf15a7d8766
Marcio <mdam@umich.edu>
2016-11-02 15:33:19 -0400

    Matlab workshop files

---
+++ MATLAB/mysquare.m
@@ -0,0 +1,14 @@
+function [x,y] = mysquare(x,aux2)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+    
+    function z=aux(x)
+        z=x^2;
+    end
+    
+    w=4;
+
+    y=aux2(x);
+    
+end
+
